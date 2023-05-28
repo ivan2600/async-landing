f4d56224d50ae73fa7c5ded93085651359ebd169
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-05-27 21:09:19 -0300

    generator

---
+++ src/assets/main.js
@@ -0,0 +1,46 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCLVz1B001PIbq9LliJenV2w&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5ee9ceb031msh604a702964eb3a9p187a19jsn44b3eabe49ed',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlAPI) {
+  const response = await fetch(urlAPI, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items.map(video => `
+    <div class="group relative">
+      <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+        <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+      </div>
+      <div class="mt-4 flex justify-between">
+        <h3 class="text-sm text-gray-700">
+          <span aria-hidden="true" class="absolute inset-0"></span>
+          ${video.snippet.title}
+        </h3>
+      </div>
+    </div>`
+    ).slice(0,4).join('')}`;
+  } catch {
+
+  }
+})();
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
